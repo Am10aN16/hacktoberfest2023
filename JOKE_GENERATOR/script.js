aeb04409a78434413598fbe6b7a924a42f49d9d4
AdityaGaur7 <adgaur027@gmail.com>
2023-10-25 19:34:52 +0530

    JOKE_GENERATOR ADDED

---
+++ JOKE_GENERATOR/script.js
@@ -0,0 +1,127 @@
+var jokes = [
+
+    "एक लड़का अपनी गर्लफ्रेंड से बोला, तुम्हारी सोच बड़ी होगी तुम कैसे मुझसे प्यार करती हो? उसने जवाब दिया, मैंने सोचा कि तुम्हें एक बार मेरे साथ घूमने के बाद तुम खुद ही समझ जाओगी कि मैं बेवकूफ हूँ।",
+    "पत्नी: तुम्हारा मुंह इतना काला क्यों है? पति: मुझे बार-बार जहर खिलाने से हो गया है।",
+    "एक आदमी ने एक दुकान में एक मुर्गी खरीदी और पूछा, बच्चा किस तरह से पैदा करता है? दुकानदार बोला, आप ने मुर्गी खरीदी है या कुछ और? आदमी बोला, नहीं, मुझे एक आदमी चाहिए था जो इसका अंडा बनाता हो।",
+    "टीचर: अगले हफ्ते आपकी परीक्षा होगी। छात्र: सर, कृपया मुझे पिछले साल की परीक्षा दीजिए। टीचर: क्यों? छात्र: मैंने उस समय उसी टाइम में एक लड़की से प्यार किया था जिस समय परीक्षा थी।"
+    , "When you finally fix a bug after hours of debugging: 'I am the master of the universe!'", "When you realize you've been coding for 8 hours straight without taking a break: 'I'm not addicted to coding, I can stop anytime I want... after this one last bug fix.'", "When you finish a project and your code works perfectly: 'I have achieved the impossible! I am a coding god!'", "When your code works in testing but breaks in production: 'I guess I'll just have to write another bug fix for my bug fix.'", "When you're working on a project with a tight deadline: 'I don't always code, but when I do, I do it under immense pressure and with limited time.'", "When you finally figure out what's wrong with your code after staring at it for hours: 'I have uncovered the mysteries of the universe!'", "When you see someone else's poorly written code: 'I don't always write perfect code, but when I do, I make sure to judge other people's code.'", "When you accidentally delete a crucial line of code: 'My code! My beautiful code! What have I done?!'", "When you finally get your code to compile after multiple attempts: 'It's not a bug, it's a feature.'",
+    "When you're trying to explain your code to someone who doesn't understand it: 'It's like trying to explain color to a blind person.'"
+    , "When you can't stop thinking about that special someone: 'My brain is 90% thoughts of you and 10% thoughts of coding.'", "When you finally find your soulmate: 'I've finally found the missing semicolon in my code.'", "When you're in love and it shows: 'You are the CSS to my HTML.'", "When you're in a long-distance relationship: 'Distance is just a number when you have Git.'", "When you find someone who loves programming as much as you do: 'We can code together forever.'", "When you're coding and your significant other walks in: 'I'm not ignoring you, I'm just in a deep coding trance.'", "When you're so in love you forget to eat: 'I've been coding all day, and all I can think about is you.'", "When you've found the one: 'You're not just my code, you're my compiler.'", "When you're in love with a programmer: 'I'm fluent in C++, but when it comes to you, I'm speechless.'", "When you're in love with a gamer: 'I'm always player two to your player one.'", "When you're in love and your code is finally bug-free: 'Nothing compares to the feeling of love... except maybe a perfectly functioning code.'", "When you're so in love it hurts: 'My heart beats for you like an infinite loop.'", "When you're in love and you just can't hide it: 'My love for you is like an open source project - anyone can see it.'", "When you're in love and you want the whole world to know: 'printf('I love you!')'", "When you're so in love you can't concentrate on coding: 'My love for you is like an infinite recursion - it never ends.'", "When you're in love and it's all you can think about: 'I'd rather be coding with you than coding alone.'", "When you're in love and everything just feels right: 'You complete me like a perfectly executed code.'", "When you're in love and everything just falls into place: 'Our love is like a well-written algorithm - it always produces the right result.'", "When you're in love and it's the best feeling in the world: 'I'd rather debug code with you than live without you.'",
+    "When you're in love and it's the greatest adventure: 'Life is an adventure, and I want to explore it with you.'"
+    , "When you've been studying for hours and you still don't understand the material: 'This is why I hate school.'", "When the teacher says 'pop quiz': 'May the odds be ever in your favor.'", "When the teacher assigns a group project: 'Why do we have to work in groups? I hate people.'", "When you get an A on an exam you didn't study for: 'The curve was definitely in my favor.'", "When you're studying and someone asks if you want to hang out: 'I can't, I have to study. But thanks for reminding me of my misery.'", "When you're the first one to finish an exam: 'Either I'm a genius or I missed something important.'", "When you're late to class and the teacher gives you a dirty look: 'I'd rather be sleeping.'", "When you get paired with your crush for a project: 'This is it. This is my chance.'", "When the teacher assigns homework over the weekend: 'I thought weekends were for fun, not for suffering.'", "When you finally finish a group project: 'I did all the work, but at least it's over.'", "When the teacher cancels class: 'Hallelujah! I can finally catch up on my sleep.'", "When you're cramming for an exam and your brain can't take it anymore: 'Error 404: Brain not found.'", "When you're taking a test and the person next to you is breathing too loudly: 'Can you please stop? You're distracting me.'", "When you're trying to stay awake during a boring lecture: 'I'm not sleeping, I'm just resting my eyes.'", "When you're taking a test and you're not sure of the answer: 'Eeny, meeny, miny, moe...'", "When you realize you forgot your homework: 'Well, looks like I'm failing this class.'", "When the teacher says 'I'm not angry, I'm just disappointed': 'I'd rather you be angry.'", "When you finish a presentation and the class claps: 'I'd like to thank my procrastination for making this possible.'", "When you're trying to study but your phone keeps distracting you: 'Why is everything more interesting than studying?'",
+    "When you realize you have a paper due tomorrow: 'Panic mode activated.'"
+
+    , "Santa: Banta, agar main mar jau toh tum kya karoge?      Banta: Main tere qadmon mein jaan de dunga.      Santa: Wah, kya dosti hai!      Banta: Haan, warna kambakht police itni aasani se mera kya haal karti.",
+    "Santa: Banta, tu bachpan mein kya banega?      Banta: Pehle mein toh doctor banunga.      Santa: Fir kya hoga?      Banta: Fir mujhe bhi apna kaam chhodna hoga aur doctor ko follow karna hoga.",
+    "Santa: Banta, tu pichle janam mein kya tha?      Banta: Mein truck driver tha.      Santa: Toh is janam mein kya hua?      Banta: Abhi tak mein steering khinch raha hu.",
+    "Santa: Banta, tu har cheez mein late kyun aata hai?      Banta: Woh mein aapko later batata hu.",
+    "Santa: Banta, tu kaun sa phone use karta hai?      Banta: Nokia ka 1100.      Santa: Abhi tak 1100 use karta hai? Tu aur tera phone dono antique pieces ho gaye hai.",
+    "Santa: Banta, tu khud ko kya samajhta hai?      Banta: Mein toh India ka Einstein hun.      Santa: Einstein? Matlab tu kya kar leta hai?      Banta: Bas, mein bhi unki tarah sar jhuka ke rehta hun.",
+    "Santa: Banta, agar tu chup rehta toh bahut accha hota.      Banta: Tum chup rehne ki practice karo phir mujhe batao.",
+    "Santa: Banta, tu jab kabhi tatti pe jaata hai toh kya teri kamar nahi dukhti?      Banta: Nahi, mere paas accha technique hai. Main table par pair rakh ke jaata hun.",
+    "Santa: Banta, tu itni badi car kyun khareed ke rakhta hai?      Banta: Bhai, main sirf ek gaadi khareedne waala hun. Chhote chhote logon ko koi farak nahi padta.",
+    "Santa: Banta, tu shakal se toh bahut innocent lagta hai.      Banta: Woh toh hai, main innocent hi hun.      Santa: Innocent hone ka kya faida hai?      Banta: Faayda nahi hai, par mujhe koi shaq nahi karta.",
+    "Santa: Banta, tu toh apni biwi ko bahut pyar karta hai na?      Banta: Haan, bas uska haath pakad ke rakhna hota hai. Kabhi haath chhoot gaya toh phir biwi bacha kya jaye.",
+    "Santa: Banta, teri biwi bahut chidchidi rehti hai.      Banta: Haan yaar, ab toh maine usse apna pura kaam sikhaya hai. Ab woh sirf chidh chidh ke paas baithegi."
+    ,
+    "Santa: Banta, tu apni biwi ko kaisi izzat deta hai?      Banta: Bas yehi kehta hun ki Madam ji aapka order ready hai.",
+    "Santa: Banta, teri aankhen kitni khoobsurat hai.      Banta: Mujhe toh nahi lagta, mein toh chashme waala hun.      Santa: Par chashme waale bhi toh khoobsurat hote hai.      Banta: Haan, par unki aankhein dikhayi nahi deti.",
+    "Santa: Banta, tu kitna padha likha hai?      Banta: Mein toh graduate hun.      Santa: Graduation ka matlab kya hai?      Banta: Pata nahi bhai, bas sab log kehte the ki graduation kar lo.",
+    "Santa: Banta, tu raat ko kya karta hai?      Banta: Bas itna hi kehta hun ki Goodnight Siri, Goodnight Google, Goodnight Alexa.",
+    "Santa: Banta, teri shadi ke baad kya kya change hua?      Banta: Bas ek hi change hua ki ab mein apne phone ke wallpaper mein apni biwi ki photo rakhta hun.",
+    "Santa: Banta, tere ghar mein TV hai?      Banta: Haan, ek toh hai.      Santa: Toh tu uska use karta hai?      Banta: Nahi yaar, woh toh bass chhat ko sambhalne ke kaam aata hai.",
+    "Santa: Banta, tu aaj kal gym jaata hai?      Banta: Haan, woh toh doctor ne mana kiya hai ki khana jyaada khaana band kar diya hai toh wazan kam karne ke liye kuch karna padega.",
+    "Santa: Banta, tu apne baal kaise rakhta hai?      Banta: Main toh aaj-kal wig pehenta hu.      Santa: Kyun?      Banta: Kyunki unke baal mere baalon se jyaada natural lagte hai.",
+    "Santa: Banta, tu bahut intelligent hai.      Banta: Haan, meri IQ bhi 140 hai.      Santa: IQ matlab kya hai?      Banta: Pata nahi, par meri biwi kahti hai ki itni intelligent hone ke bawajood bhi mein uske har sawaal ka jawaab nahi de pata.",
+    "Santa: Banta, teri biwi kaun si cream use karti hai?      Banta: Pata nahi yaar, par mere wallet se kuch kaante hamesha kam nahi hote hai."
+    ,
+    "Pati: Tumhara chehra bilkul solar panel jaisa hai.      Pati Patni: Kyun?      Pati: Kyunki jab tum mere paas aati ho toh meri life ka energy level badh jaata hai.",
+    "Pati: Tum mujhse kitna pyaar karti ho?      Pati Patni: Jab main tumhe meri zindagi ka hissa kehti hu, toh yeh bilkul sach nahi hai. Tum meri zindagi hi ho.",
+    "Pati: Kya tumne koi kaam kiya hai aaj?      Pati Patni: Haan, maine tumhara chai banaaya hai.      Pati: Maine toh kabhi nahi kaha ki tumhe chai banaani chahiye.      Pati Patni: Haan, par maine socha ki tum aaj kaam par jaaoge, isliye bana diya.",
+    "Pati Patni ek dusre se door hote hue bhi baat kar rahe the.      Pati: Tumne mere liye kuch khaas kiya hai?      Pati Patni: Haan, maine tumhari photo khinch ke Whatsapp status mein lagayi hai.",
+    "Pati: Tumhari aankhen kitni pyaari hai.      Pati Patni: Aaj kal toh bas tumhare paas hi hote hai, isliye.",
+    "Pati: Main kabhi nahi chahunga ki hum dono ke beech koi problem ho.      Pati Patni: Toh phir kya hua?      Pati: Kuch nahi, bas yeh sunna tha ki aaj kal tumhaare kharche jyaada ho rahe hai.",
+    "Pati: Tumhara weight badh gaya hai.      Pati Patni: Tum bhi toh theek nahi dikh rahe ho.      Pati: Par maine toh kabhi kuch nahi kaha tumhare baare mein.      Pati Patni: Haan, par maine kaha hai.",
+    "Pati Patni ek restaurant mein dinner kar rahe the.      Pati: Tumne kya khaya?      Pati Patni: Tumhare paise se khaya hai toh bataana padega?",
+    "Pati: Tumne aaj mere liye kuch banaaya hai?      Pati Patni: Haan, ek surprise hai aaj dinner mein.      Pati: Kya?      Pati Patni: Main khaana order karungi.",
+    "Pati Patni shopping mall mein hai.      Pati: Tum kuch chahiye?      Pati Patni: Nahi, bas dekh rahi thi.      Pati: Lekin tum toh ab tak yeh bhi nahi dekhi ki kitni lambi line hai billing counter par.",
+    "Pati Patni ka phone chori ho jaata hai.      Pati Patni: Mera phone kahan hai?      Pati: Pata nahi, mere paas nahi hai.      Pati Patni: Tumne kahi rakh diya hoga.      Pati: Nahi yaar, maine toh tumhara phone hi le liya tha."
+    , "When you spend all night debugging and finally fix the issue:", "When you finally understand that one concept that has been eluding you for weeks:", "When you have a deadline in two hours but your code still won't compile:", "When your code works perfectly in your development environment but breaks in production:", "When you finally finish a project that you've been working on for months:", "When you realize you've been coding for 10 hours straight and forgot to eat:", "When you have to explain your code to someone who isn't a programmer:", "When you're the only one who knows how to fix a bug and everyone keeps bugging you about it:", "When you realize you've been using the wrong variable name the whole time:", "When you finally finish a project and realize you forgot to comment any of your code:", "When you're trying to work but your computer is so slow that you have time to make coffee while it loads:", "When you finally get a good grade on a project that you've been stressing about for weeks:", "When you're trying to learn a new programming language and it feels like you're starting from scratch:", "When you finally get a job offer after months of job searching:", "When your code finally runs without any errors:", "When you realize you've been coding for so long that you forgot what day it is:", "When you're the only one in the group project who actually did any work:", "When you realize you've been using an outdated library this whole time:", "When you finally understand the joke in a programming meme:",
+    "When someone asks you what you do for a living and you have to explain that you're an engineer:"
+    ,
+    "Papa beta ek restaurant mein baithe hain, papa kehte hain 'kya khayoge beta?', beta kehta hai 'Jo aapne order kiya hai wahi'.",
+    "Ek din beta papa ke saath ghumne gaya, papa ne pucha 'beta jaha chaho waha ghum sakte hain', beta bola 'to phir moon par chalte hain'.",
+    "Papa beta ke padosi se puchte hain 'beta yeh aapki cycle hai?', beta kehta hai 'nahi papa, yeh toh cycle ki tasveer hai'.",
+    "Papa beta se puchte hain 'beta, ye tumhare mobile mein jo photo hai, ye kiski hai?', beta kehta hai 'papa, ye toh stock photo hai'.",
+    "Papa beta se puchte hain 'beta, tumhari favourite subject kya hai?', beta kehta hai 'subjectivity papa'.",
+    "Papa beta ko school chhodne ke liye lekar jaa rahe hain, papa kehte hain 'beta tum kitni der school mein padhte ho?', beta kehta hai 'bas papa, lunch break se pehle'.",
+    "Ek din beta papa ke saath cricket khelne gaya, papa ne bola 'beta aapko batting ya bowling karna hai?', beta bola 'papa main toh fielding karunga'.",
+    "Papa beta ko car mein lekar jaa rahe hain, papa kehte hain 'beta seat belt pehno', beta kehta hai 'papa maine toh invisible seat belt pehni hai'.",
+    "Papa beta se puchte hain 'beta tumhari girlfriend kaisi hai?', beta kehta hai 'papa meri toh coding hi girlfriend hai'.",
+    "Papa beta ek bike ride par ja rahe hain, papa kehte hain 'beta speed limit ka khayal rakho', beta kehta hai 'papa, maine toh time limit set kar li hai'.",
+    "Ek din papa beta ko kaafi daant rahe the, beta kehta hai 'papa, aap jaise jaise bura bolte hain, main apni coding skills ko improve kar leta hoon'.",
+    "Papa beta se puchte hain 'beta, tumhara future plan kya hai?', beta kehta hai 'papa, maine toh apna career kaafi saal pehle hi hack kar liya hai'.",
+    "Papa beta ek party mein hain, papa kehte hain 'beta dance karoge?', beta kehta hai 'nahi papa, main toh DJ banunga'.",
+    "Ek din beta papa ko kaafi tang kar raha tha, papa kehte hain 'beta, maine tumhe kaafi daant di hai', beta kehta hai 'papa, woh toh aapka bug tha'.",
+    "Papa beta se puchte hain 'beta, tumne aaj kya seekha?', beta kehta hai 'papa, maine seekha hai ki zindagi ek endless loop hai'.",
+
+    "Pagal dost se kehte hain 'tumhare paas sabse badi taakat kaunsi hai?', dost kehta hai 'meri pagalpan ki taakat'.",
+    "Ek din pagal dost ne apni photo khichai, usne bola 'ye photo insta pe daalunga', maine pucha 'kyun?', usne bola 'main khud follow karta hoon'.",
+    "Pagal dost se puchte hain 'tumhari favourite language kaunsi hai?', dost kehta hai 'pagalpanti meri mother tongue hai'.",
+    "Ek din main pagal dost ke saath ghumne gaya, usne mujhse kaha 'mere paas tumhari aur meri ticket hai, tumhe apni ticket kahaan hai?', maine kaha 'meri ticket toh tumhare paas hi hai'.",
+    "Pagal dost kehte hain 'tumhe koi kaam hai?', maine bola 'haan, ek article likhna hai', dost bola 'tu bhi na, saari zindagi likhte hi rahega'.",
+    "Ek din maine pagal dost ko bola 'chal ek game khelte hain', dost bola 'chalo', maine kaha 'game ka naam hai patience', dost bola 'nahi yaar, mujhe abhi ghar jaana hai'.",
+    "Pagal dost ko puchte hain 'tu itna kyun udas rehta hai?', dost bola 'duniya itni khubsurat hai, main usse control nahi kar pata'.",
+    "Ek din maine pagal dost se kaha 'tum itni raat ko kahaan ja rahe ho?', dost bola 'sone'.",
+    "Pagal dost se puchte hain 'tumhe kya pasand hai?', dost bola 'main pagal hoon, mujhe kuch pasand nahi hai'.",
+    "Ek din maine pagal dost ko pucha 'tumhare paas kitne followers hai?', dost bola 'followers nahi hai, par coding skills hai'.",
+    "Pagal dost ko puchte hain 'tumhare paas girlfriend hai?', dost bola 'nahi hai, par meri coding skills hai'.",
+    "Ek din pagal dost ne apni bike par ride karne ke liye kaha, maine kaha 'par tumhari bike toh break fail hai', dost bola 'oh no, ye toh meri favourite feature hai'.",
+    "Pagal dost se puchte hain 'tumhari pocket mein kya hai?', dost kehta hai 'meri pagalpan ki bhara hua hai'.",
+    "Ek din maine pagal dost se kaha 'chal cinema dekhne jaate hain', dost bola 'chal, par tickets toh mere paas nahi hai', maine kaha 'toh kya karoge?', dost bola 'main hero ke role mein apne aap ko imagine kar loonga'.",
+    "Pagal dost ko puchte hain 'tumhari job kya hai?', dost bola 'mujhe kya pata, main toh pagal hoon'."
+];
+
+const jk = document.getElementById("joke");
+const button = document.getElementById("btn");
+
+
+var gifs=[
+    "img/2.gif","img/3.gif","img/4.gif","img/4.jpeg","img/5.gif","img/5.png",
+    "img/6.gif","img/6.png","img/7.png","img/8.png","img/8.jpeg","img/9.gif",
+    "img/11.gif","img/14.gif","img/17.gif","img/200w.gif","img/a.gif","img/d.gif"
+    ,"img/e.gif","img/f.gif","img/i.gif","img/q.gif","img/q.webp","img/qa.gif"
+    ,"img/qw.gif","img/r.png","img/s.gif","img/t.gif","img/u.gif","img/w.gif"
+    ,"img/i.gif","img/f.gif","img/d.gif","img/y.jpeg","img/w.gif","img/u.jpeg"
+    ,"img1/as.gif" ,"img1/b.gif","img1/c.gif","img1/ct.gif","img1/n.gif","img1/n.gif"
+    ,"img1/v.gif","img1/x.gif","img1/z.gif","img1/za.gif","img1/zd.gif","img1/zf.gif","img1/zs.gif"
+]
+const image=document.getElementById("image").src;
+
+
+
+
+function f() {
+    let random = Math.floor(Math.random()*jokes.length);
+   
+    jk.innerText = jokes[random];
+    let no = Math.floor(Math.random()*gifs.length);
+
+    img.src = gifs[no];
+}
+
+const img = document.createElement('img');
+
+
+document.body.appendChild(img);
+
+
+
+
+
+
+
+
+
+
+
